6f955ce9f1ff880532c529bbabad27980b7f5c9a
Bruno Wanderson <brunowanderson8@gmail.com>
2023-11-16 15:53:00 -0300

    feat: fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,23 @@
-import { Groups } from '@screens/Groups'
 import { ThemeProvider } from 'styled-components'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
 
+import { Loading } from '@components/Loading'
+import { Groups } from '@screens/Groups'
 import theme from '@theme/index'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  })
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   )
 }
