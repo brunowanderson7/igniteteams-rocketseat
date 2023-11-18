6883ce3725923b9702b0c10afb30608c3357c455
Bruno Wanderson <brunowanderson8@gmail.com>
2023-11-18 19:22:13 -0300

    feat: component highlight

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
 import * as S from './styles'
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+      <Highlight
+        title="Turma"
+        subtitle="Jogue com sua turma"
+      />
     </S.Container>
   )
 }
